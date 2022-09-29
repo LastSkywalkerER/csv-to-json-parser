8795420e7597072739b37c2bd84e83b534a158d1
LastSkywalkerER <maxdr1998@gmail.com>
2022-09-29 13:59:41 +0300

    add events

--- application/commands/main.ts
+++ application/commands/main.ts
@@ -1,23 +1,52 @@
 import * as child_process from 'child_process';
+import { EventEmitter } from 'events';
 import * as fs from 'fs';
 import * as stream from 'stream';
 
 import { eol, options } from './constants';
 
+class ProcessEmitter extends EventEmitter {}
 interface CommandsProps extends Record<string, unknown> {
+  processEmitter: ProcessEmitter;
   separator: string;
 }
 
 class Commands extends stream.Transform {
   separator: string;
   jobCounter: number;
+  processEmitter: ProcessEmitter;
 
-  constructor({ separator, ...restOptions }: CommandsProps = { separator: '\r\n' }) {
+  constructor({ processEmitter, separator, ...restOptions }: CommandsProps) {
     super({ ...restOptions, decodeStrings: false });
 
     this.separator = separator;
     this.jobCounter = 0;
+    this.processEmitter = processEmitter;
+
+    this.processEmitter.on('execute', this.processCallback);
+    this.processEmitter.on('closeProcess', this.checkClose);
   }
+
+  checkClose = (commands: string[], callback: stream.TransformCallback) => {
+    if (this.jobCounter === commands.length) {
+      this.jobCounter = 0;
+      callback();
+    }
+  };
+
+  processCallback = (element: string, index: number, error: child_process.ExecException, stdout: string, stderr: string) => {
+    this.jobCounter++;
+
+    if (error || stderr) {
+      console.error(error || stderr);
+      this.push(`job ${++index} - ${element} error ${error || stderr}`);
+
+      return;
+    }
+
+    console.log(stdout);
+    this.push(`job ${++index} - ${element} output: ${stdout ? stdout : 'success'}`);
+  };
 
   _transform(chunk: string, encoding: BufferEncoding, callback: stream.TransformCallback): void {
     if (encoding !== 'utf8') {
@@ -28,43 +57,13 @@ class Commands extends stream.Transform {
 
     const commands = chunk.split(eol);
 
-    const checkExit = () => {
-      if (this.jobCounter === commands.length) {
-        this.jobCounter = 0;
-        callback();
-      }
-    };
-
-    const processCallback =
-      (element: string, index: number) => (error: child_process.ExecException, stdout: string, stderr: string) => {
-        this.jobCounter++;
-
-        if (error || stderr) {
-          console.error(error || stderr);
-          this.push(`job ${++index} - ${element} error ${error || stderr}`);
-
-          return;
-        }
-
-        console.log(stdout);
-        this.push(`job ${++index} - ${element} output: ${stdout ? stdout : 'success'}`);
-      };
-
     commands.forEach((element, index) => {
-      const process = child_process.exec(element, processCallback(element, index));
-
-      // process.stdout.on('data', (data) => {
-      //   console.log(data.toString('utf8'));
-      //   this.push(`job ${++index} - ${element} output: ${data ? data.toString('utf8') : 'success'}`);
-      // });
-
-      // process.stderr.on('error', (error) => {
-      //   console.error(error);
-      //   this.push(`job ${++index} - ${element} error ${error}`);
-      // });
+      const process = child_process.exec(element, (error: child_process.ExecException, stdout: string, stderr: string) =>
+        this.processEmitter.emit('execute', element, index, error, stdout, stderr),
+      );
 
       process.on('close', () => {
-        checkExit();
+        this.processEmitter.emit('closeProcess', commands, callback);
       });
     });
   }
@@ -74,7 +73,7 @@ const start = ({ resultFile, separator, sourceFile }: typeof options) => {
   const readStream = fs.createReadStream(sourceFile, { encoding: 'utf-8' });
   const writeStream = fs.createWriteStream(resultFile, { encoding: 'utf-8' });
 
-  readStream.pipe(new Commands({ separator })).pipe(writeStream);
+  readStream.pipe(new Commands({ separator, processEmitter: new ProcessEmitter() })).pipe(writeStream);
 };
 
 start(options);
